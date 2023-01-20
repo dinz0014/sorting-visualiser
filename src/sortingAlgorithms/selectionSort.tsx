32cde45226a14ccd79695951f7daecec419be90e
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2023-01-20 16:19:41 +1100

    Added selection sort code which sorts the array and also outputs a list of animations to be displayed.

--- src/sortingAlgorithms/selectionSort.tsx
+++ src/sortingAlgorithms/selectionSort.tsx
@@ -1,25 +1,47 @@
 import { animation } from '../types/sortVisualiserTypes';
+import { animationType } from '../types/sortVisualiserTypes';
 
 export default function getSelectionSortAnimations(
-    array: number[]
+    originalArray: number[]
 ): animation[] {
     const animations = [];
+    const copiedArray = Object.assign([], originalArray);
 
-    for (let i = 0; i < array.length; i++) {
-        let currMin = array[i];
+    for (let i = 0; i < copiedArray.length; i++) {
+        let currMin = copiedArray[i];
         let mindex = i;
 
-        for (let j = i + 1; j < array.length; j++) {
-            animations.push({ firstIdx: mindex, secondIdx: j });
-            if (array[j] < currMin) {
-                currMin = array[j];
+        for (let j = i + 1; j < copiedArray.length; j++) {
+            animations.push({
+                type: animationType.ComparisonOn,
+                firstIdx: mindex,
+                secondIdx: j
+            });
+            animations.push({
+                type: animationType.ComparisonOff,
+                firstIdx: j,
+                secondIdx: mindex
+            });
+            if (copiedArray[j] < currMin) {
+                currMin = copiedArray[j];
                 mindex = j;
             }
         }
 
-        let temp = array[mindex];
-        array[mindex] = array[i];
-        array[i] = temp;
+        let temp = copiedArray[mindex];
+        copiedArray[mindex] = copiedArray[i];
+        copiedArray[i] = temp;
+
+        originalArray[mindex] = originalArray[i];
+        originalArray[i] = temp;
+
+        animations.push({
+            type: animationType.Swap,
+            firstIdx: i,
+            firstValue: originalArray[i],
+            secondIdx: mindex,
+            secondValue: originalArray[mindex]
+        });
     }
 
     return animations;
