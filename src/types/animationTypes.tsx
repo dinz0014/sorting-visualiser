22bbb064a5f5e534076bb9eac38d826ce1a7f860
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2023-01-24 20:56:05 +1100

    Separated animation types and the component types for props and states

---
+++ src/types/animationTypes.tsx
@@ -0,0 +1,29 @@
+export enum AnimationType {
+    ComparisonOn = 1,
+    ComparisonOff = 2,
+    Swap = 3,
+    Replace = 4
+}
+
+export type ComparisonAnimation = {
+    type: AnimationType.ComparisonOn | AnimationType.ComparisonOff;
+    firstIdx: number;
+    secondIdx: number;
+    colour: string;
+};
+
+export type SwapAnimation = {
+    type: AnimationType.Swap;
+    firstIdx: number;
+    firstValue: number;
+    secondIdx: number;
+    secondValue: number;
+};
+
+export type ReplaceAnimation = {
+    type: AnimationType.Replace;
+    idx: number;
+    value: number;
+};
+
+export type Animation = ComparisonAnimation | SwapAnimation | ReplaceAnimation;
--- src/types/sortVisualiserTypes.tsx
+++ src/types/sortVisualiserTypes.tsx
@@ -9,33 +9,3 @@ export type SortVizProps = {
 export type SortVizState = {
     currArray: number[];
 };
-
-export enum AnimationType {
-    ComparisonOn = 1,
-    ComparisonOff = 2,
-    Swap = 3,
-    Replace = 4
-}
-
-export type ComparisonAnimation = {
-    type: AnimationType.ComparisonOn | AnimationType.ComparisonOff;
-    firstIdx: number;
-    secondIdx: number;
-    colour: string;
-};
-
-export type SwapAnimation = {
-    type: AnimationType.Swap;
-    firstIdx: number;
-    firstValue: number;
-    secondIdx: number;
-    secondValue: number;
-};
-
-export type ReplaceAnimation = {
-    type: AnimationType.Replace;
-    idx: number;
-    value: number;
-};
-
-export type Animation = ComparisonAnimation | SwapAnimation | ReplaceAnimation;
